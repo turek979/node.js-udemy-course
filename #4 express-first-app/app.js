c88dd5fc881717e2c02520b679aef730a72e750b
Jakub Turowski <turek979@gmail.com>
2025-04-01 18:51:10 +0200

    add styling, public, send html files as responses, routing

--- #4 express-first-app/app.js
+++ #4 express-first-app/app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -13,9 +15,13 @@ const shopRoutes = require('./routes/shop');
 // });
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(adminRoutes);
-
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'));
+});
 
 app.listen(3000);
--- #4 express-first-app/routes/shop.js
+++ #4 express-first-app/routes/shop.js
@@ -1,9 +1,13 @@
+const path = require('path');
+
 const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
-router.use((req, res, next) => {
-    res.send('<h2>Shop route</h2>');
+router.get('/', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
