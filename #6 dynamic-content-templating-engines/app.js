044ddb5873b536375d556a4a6406ea7d6c145518
Jakub Turowski <turek.priv@gmail.com>
2025-04-02 11:25:32 +0200

    create new project, push request data to an object, add templating engines packages

--- #6 dynamic-content-templating-engines/app.js
+++ #6 dynamic-content-templating-engines/app.js
@@ -3,19 +3,19 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const userRoutes = require('./routes/users');
-const mainPageRoutes = require('./routes/main-page');
-
 const app = express();
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(mainPageRoutes);
-app.use(userRoutes);
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'))
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
