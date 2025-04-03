9b0bb8dbb5f4d3505b57e09919d8f9121b9a2559
Jakub Turowski <turek.priv@gmail.com>
2025-04-03 12:35:27 +0200

    convert html to handlebars files, render them and use layouts

--- #6 dynamic-content-templating-engines/app.js
+++ #6 dynamic-content-templating-engines/app.js
@@ -1,24 +1,44 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.set('view engine', 'pug');
-app.set('views', 'views');
+// Register pug as a templating engine, express supports it out of the box
+// app.set('view engine', 'pug');
+// app.set('views', 'views');
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// Register handlebars as a templating engine, has to be configured
+app.engine(
+  "hbs",
+  expressHbs({
+    layoutsDir: "views/layouts/",
+    defaultLayout: "main-layout",
+    extname: "hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", "views");
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+// Register ejs as a templating engine, express supports it out of the box
+// app.set('view engine', 'ejs');
+// app.set('views', 'views');
 
-app.use('/admin', adminData.routes);
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
+
+// Same rendering for pug, handlebars and ejs files
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: '404 Page Not Found'});
+  res.status(404).render("404", { pageTitle: "404 Page Not Found" });
 });
 
 app.listen(3000);
--- #6 dynamic-content-templating-engines/routes/shop.js
+++ #6 dynamic-content-templating-engines/routes/shop.js
@@ -1,15 +1,32 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+const rootDir = require("../util/path");
+const adminData = require("./admin");
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+// Rendering pug files
+// router.get("/", (req, res, next) => {
+//   const products = adminData.products;
+//   res.render("shop", {
+//     prods: products,
+//     pageTitle: "Shop",
+//     path: "/",
+//   });
+// });
+
+router.get("/", (req, res, next) => {
   const products = adminData.products;
-  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
 });
 
 module.exports = router;
