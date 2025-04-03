6afd090eaa3222b1a0922543dcc75c60bede9cfa
Jakub Turowski <turek.priv@gmail.com>
2025-04-03 13:36:08 +0200

    convert html to ejs files, render them and use includes instead of layouts

--- #6 dynamic-content-templating-engines/app.js
+++ #6 dynamic-content-templating-engines/app.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const expressHbs = require("express-handlebars");
+// const expressHbs = require("express-handlebars");
 
 const app = express();
 
@@ -11,20 +11,20 @@ const app = express();
 // app.set('views', 'views');
 
 // Register handlebars as a templating engine, has to be configured
-app.engine(
-  "hbs",
-  expressHbs({
-    layoutsDir: "views/layouts/",
-    defaultLayout: "main-layout",
-    extname: "hbs",
-  })
-);
-app.set("view engine", "hbs");
-app.set("views", "views");
+// app.engine(
+//   "hbs",
+//   expressHbs({
+//     layoutsDir: "views/layouts/",
+//     defaultLayout: "main-layout",
+//     extname: "hbs",
+//   })
+// );
+// app.set("view engine", "hbs");
+// app.set("views", "views");
 
 // Register ejs as a templating engine, express supports it out of the box
-// app.set('view engine', 'ejs');
-// app.set('views', 'views');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -38,7 +38,7 @@ app.use(shopRoutes);
 
 // Same rendering for pug, handlebars and ejs files
 app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "404 Page Not Found" });
+  res.status(404).render("404", { pageTitle: "404 Page Not Found", path: "" });
 });
 
 app.listen(3000);
