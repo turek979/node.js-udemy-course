fefe79b7d6fc71857b9efc88164177a0d06bf0a0
Jakub Turowski <turek.priv@gmail.com>
2025-04-03 09:58:18 +0200

    Use layouts in pug files, display dynamic data

--- #6 dynamic-content-templating-engines/app.js
+++ #6 dynamic-content-templating-engines/app.js
@@ -18,7 +18,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404');
+    res.status(404).render('404', {pageTitle: '404 Page Not Found'});
 });
 
 app.listen(3000);
