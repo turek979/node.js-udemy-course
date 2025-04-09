e8644cacbd82355a57dc5ea8a4e9d3d7431b1638
Jakub Turowski <turek.priv@gmail.com>
2025-04-09 11:48:12 +0200

    finish implementing MVC pattern

--- #7 MVC/controllers/products.js
+++ #7 MVC/controllers/products.js
@@ -1,4 +1,4 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
 
 exports.getAddProductPage = (req, res, next) => {
   res.render("add-product", {
@@ -14,7 +14,7 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getProductsPage = (req, res, next) => {
-  const products = Product.fetchAll((products) => {
+  Product.fetchAll((products) => {
     res.render("shop", {
       prods: products,
       pageTitle: "Shop",
--- #7 MVC/models/product.js
+++ #7 MVC/models/product.js
@@ -1,35 +1,34 @@
-const fs = require('fs');
-const path = require('path');
-const rootDir = require('../util/path');
+const fs = require("fs");
+const path = require("path");
+const rootDir = require("../util/path");
+
+const p = path.join(rootDir, "data", "products.json");
+
+const getProductsFromFile = (callback) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      return callback([]);
+    }
+    callback(JSON.parse(fileContent));
+  });
+};
 
 module.exports = class Product {
-    constructor(title) {
-        this.productTitle = title;
-    }
+  constructor(title) {
+    this.productTitle = title;
+  }
 
-    save() {
-        const p = path.join(rootDir, 'data', 'products.json');
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(fileContent);
-            }
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (err) => {
-                console.log(err);
-            });
-        });
-    }
+  save() {
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
 
-    // static allows js to refer to the whole object and not just an instanciated one you create when calling it
-    static fetchAll(callback) {
-        const p = path.join(rootDir, 'data', 'products.json');
-        fs.readFile(p, (err, fileContent) => {
-            if (err) {
-                callback([]);
-            }
-            callback(JSON.parse(fileContent));
-        });
-        
-    }
-}
+  // static allows js to refer to the whole object and not just an instanciated one you create when calling it
+  static fetchAll(callback) {
+    getProductsFromFile(callback);
+  }
+};
