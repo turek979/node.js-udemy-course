cc9919a8d7c5ac459ae7f2f720c2f43ad79180e7
Jakub Turowski <turek.priv@gmail.com>
2025-04-10 12:59:29 +0200

    product details, dynamic routing and add to cart

--- #8 dynamic-routes-advanced-models/models/product.js
+++ #8 dynamic-routes-advanced-models/models/product.js
@@ -23,6 +23,7 @@ module.exports = class Product {
     this.imageUrl = imageUrl;
     this.description = description;
     this.price = price;
+    this.id = Math.random().toString();
   }
 
   save() {
@@ -36,5 +37,12 @@ module.exports = class Product {
 
   static fetchAll(cb) {
     getProductsFromFile(cb);
+  }
+
+  static findById(id, cb){
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id);
+      cb(product);
+    })
   }
 };
