ebc5e8276f2512d2d3bb9aa97ddb301ef5b99c91
Jakub Turowski <turek.priv@gmail.com>
2025-04-11 12:58:16 +0200

    add dynamic url to editing

--- #8 dynamic-routes-advanced-models/routes/admin.js
+++ #8 dynamic-routes-advanced-models/routes/admin.js
@@ -15,6 +15,6 @@ router.get('/products', adminController.getProducts);
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
-router.get('/edit-product:productId', adminController.getEditProduct);
+router.get('/edit-product/:productId', adminController.getEditProduct);
 
 module.exports = router;
